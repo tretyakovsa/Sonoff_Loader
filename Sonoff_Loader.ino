63e12b3dcb1923f4156b985d663b9fce8c0879d4
Unknown <tretyakov.sa@gmail.com>
2017-10-12 03:46:27 +0800

    build 2017.10.11

--- Sonoff_Loader.ino
+++ Sonoff_Loader.ino
@@ -35,12 +35,12 @@ void webUpdate() {
   // отключаем перезагрузку после обновления FS
     ESPhttpUpdate.rebootOnUpdate(false);
   //Обнавляем FS
-  t_httpUpdate_return ret = ESPhttpUpdate.updateSpiffs("http://backup.privet.lv/esp/sonoff/spiffs.0xBB000_flash_size_1Mb.256Kb_2017.09.20.bin");
+  t_httpUpdate_return ret = ESPhttpUpdate.updateSpiffs("http://backup.privet.lv/esp/sonoff/spiffs.0xBB000_flash_size_1Mb.256Kb_2017.10.11.bin");
   // включаем перезагрузку после прошивки
   digitalWrite(LED_PIN, HIGH);
   ESPhttpUpdate.rebootOnUpdate(true);
   // Перепрошиваем модуль
-  t_httpUpdate_return ret1 = ESPhttpUpdate.update("http://backup.privet.lv/esp/sonoff/build.0x00000_flash_size_1Mb.256Kb_2017.09.20.bin");
+  t_httpUpdate_return ret1 = ESPhttpUpdate.update("http://backup.privet.lv/esp/sonoff/build.0x00000_flash_size_1Mb.256Kb_2017.10.11.bin");
 }
 
 
